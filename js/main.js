5e4b145f46234b4c6c5621ebf2295818df56b134
Mohammed Jaseel <jaseel2694@gmail.com>
2018-02-14 19:05:48 +0530

    Changes to be committed:

---
+++ js/main.js
@@ -0,0 +1,46 @@
+/*===============
+   MENU OVERLAY
+==================*/
+
+function openNav() {
+    document.getElementById("nav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("nav").style.width = "0%";
+}
+
+/*===============
+   MAP
+==================*/
+
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(10.872412, 76.287393),
+        zoom: 5,
+    };
+
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+}
+
+/*===============
+  SCROLL UP
+==================*/
+
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("scrollup").style.display = "block";
+    } else {
+        document.getElementById("scrollup").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
