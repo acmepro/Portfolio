95fdd75a43ca70ba43499602ec9c49a2a76196f1
Mohammed Jaseel <jaseel2694@gmail.com>
2018-03-13 12:34:41 +0530

    Changes to be committed: 	modified:   css/style.css 	modified:   index.html 	new file:   js/scroll.js

---
+++ js/scroll.js
@@ -0,0 +1,60 @@
+window.sr = ScrollReveal();
+        sr.reveal('.home-content', {
+            duration: 2000,
+            origin: 'bottom',
+            distance: '200px'
+        });
+
+        sr.reveal('.about-heading', {
+            duration: 2000,
+            origin: 'bottom',
+            distance: '200px',
+            scale: 0.1
+        });
+
+        sr.reveal('.about-content', {
+            duration: 3000,
+            origin: 'bottom',
+            distance: '200px'
+        });
+
+        sr.reveal('.services-heading', {
+            duration: 2000,
+            origin: 'bottom',
+            distance: '200px',
+            scale: 0.1
+        });
+
+          sr.reveal('.services-content', {
+            duration: 2500,
+            origin: 'bottom',
+            distance: '100px'
+        });
+
+          sr.reveal('.skills-heading', {
+            duration: 2000,
+            origin: 'bottom',
+            distance: '200px',
+            scale: 0.1
+        });
+
+        sr.reveal('.contact-box h2', {
+            duration: 2000,
+            origin: 'left',
+            distance: '200px',
+            scale: 0.1
+        });
+
+        sr.reveal('.contact-box p', {
+            duration: 2000,
+            origin: 'right',
+            distance: '200px',
+            scale: 0.1
+        });
+
+         sr.reveal('.form-heading', {
+            duration: 2000,
+            origin: 'bottom',
+            distance: '200px',
+            scale: 0.1
+        });
